e96dd6dfd8d4c46483984afc4239d9c28618678d
anisaalhaqi <anisaalhaqi@gmail.com>
2025-11-24 13:35:06 +0700

    refactor: add new getAdjacent method and add constructor

--- src/java/model/Chef/Position.java
+++ src/java/model/Chef/Position.java
@@ -1,14 +1,25 @@
 package java.model.Chef; 
 
 public class Position {
-    private int x;
-    private int y;
+    private final int x;
+    private final int y;
 
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
     public int getX() {
         return x;
     }
     
     public int getY() {
         return y;
+    }
+
+    public static Position getAdjacent(Position current, Direction direction) {
+        int newX = current.getX() + direction.getX();
+        int newY = current.getY() + direction.getY();
+
+        return new Position(newX, newY);
     }
 }
